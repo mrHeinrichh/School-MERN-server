198163370373741c6a546345c910a854dab771dd
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-01-25 10:23:39 +0800

    Add post js routes

---
+++ routes/post.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+// import controller methods
+
+const { create } = require('../controllers/post')
+
+router.post('/post', create)
+
+module.exports = router
